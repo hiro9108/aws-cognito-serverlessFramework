653100b90827e93d7babfa67ce3ea38a38394339
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-05-09 04:02:11 -0700

    check: nextauth with cognito

---
+++ client/pages/api/auth/[...nextauth].js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.Cognito({
+      clientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET,
+      domain: process.env.NEXT_PUBLIC_COGNITO_DOMAIN,
+    }),
+  ],
+});
