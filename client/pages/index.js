11fde5a34d5ce8713119315c65c503b755e297f2
hiro9108 <hiroshi.8.egawa@gmail.com>
2021-05-12 20:31:43 -0700

    add: github flows

--- client/pages/index.js
+++ client/pages/index.js
@@ -16,14 +16,11 @@ export default function Home() {
   // };
 
   const onClickGetDataHandler = async () => {
-    const { data } = await axios.get(
-      "https://1lvrln5vkb.execute-api.ca-central-1.amazonaws.com/dev/hello",
-      {
-        headers: {
-          Authorization: `Bearer eyJraWQiOiJ3eGFWSXlyVXdKaU9VeXZxaHRUWW92QTRLS1hNZ0VRSkNCaDhJcTRvcWhnPSIsImFsZyI6IlJTMjU2In0.eyJhdF9oYXNoIjoiand2cWV6R3R6cWlNenU2S2hrT0R5USIsInN1YiI6IjU2M2ZjNzU3LWQ0ZjctNGI5ZS1hMmQ1LTYzMDhiMzI4ZmQ2NyIsImF1ZCI6IjNpZDg1amppZzEwMGUwMGNxZzNldmszOTgyIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjIwODU1NTkyLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuY2EtY2VudHJhbC0xLmFtYXpvbmF3cy5jb21cL2NhLWNlbnRyYWwtMV85RHdiRzJiUmkiLCJjb2duaXRvOnVzZXJuYW1lIjoiNTYzZmM3NTctZDRmNy00YjllLWEyZDUtNjMwOGIzMjhmZDY3IiwiZXhwIjoxNjIwODU5MTkyLCJpYXQiOjE2MjA4NTU1OTIsImVtYWlsIjoiaGlyb3NoaS44LmVnYXdhQGdtYWlsLmNvbSJ9.GrSbs9E_T3fHwqF4gxDVtz7BwtRAwb5pDIK08XkrRUaeAymsaBfxxr80SD_otgDiJQMogim5E8ZY236YE5cqqu-EIeynn6YxEnCCkqRXDdlrEGcHyhD6WXQ6wNvLsj6TFGpkSDpgK4NwCgAlbohCKyo62Eei2wfQhHvsQFZrgRAqbYAOOVYV1H2ayP8erfg7C3Mert0pUZgn73nOAhan4QD51Yze-Lbn8rknXPvyhJdT0iZhtfNmV1IlkGuhA_Ye2wwMnYJmIrYMMpFVr1GL-iKWsa5DJMkasa4SA_CFc4m-5OeTVfFYzlGzqXr_1Smd8UVcsfnQu2MOllEalhPAlg`,
-        },
-      }
-    );
+    const { data } = await axios.get("https://xxx", {
+      headers: {
+        Authorization: `Bearer `,
+      },
+    });
     console.log(data);
   };
 
